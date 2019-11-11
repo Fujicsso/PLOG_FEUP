aad05785872483d85d7296bee9a55a48118f7166
Mark Meehan <mtimothy38@gmail.com>
2019-11-11 22:54:25 +0000

    implemented correct code structure

--- P1/src/main.pl
+++ P1/src/main.pl
@@ -3,13 +3,11 @@
 :- include('utils.pl').
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
-:- use_module(library(between)).
+% :- use_module(library(between)).
 :- use_module(library(aggregate)).
 :- use_module(library(random)).
 :- use_module(library(system)).
 
 :- dynamic state/3.
 
-menu:- mainMenu1.
-
-startGame:- startGameP.
+play:- mainMenu.
